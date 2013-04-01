3d185f40be1bdaf87498a8c6fc23752d1f192de8
Shawn Nock <nock@nocko.se>
2013-04-01 08:48:31 -0400

    Adds passives and first try board layout

---
+++ nRF51.cmd
@@ -0,0 +1,158 @@
+# Pin name action command file
+
+# Start of element CONN3
+ChangePinName(CONN3, 7, 7)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 8, 8)
+ChangePinName(CONN2, 7, 7)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+
+# Start of element CONN5
+ChangePinName(CONN5, 8, 8)
+ChangePinName(CONN5, 7, 7)
+ChangePinName(CONN5, 6, 6)
+ChangePinName(CONN5, 5, 5)
+ChangePinName(CONN5, 4, 4)
+ChangePinName(CONN5, 3, 3)
+ChangePinName(CONN5, 2, 2)
+ChangePinName(CONN5, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element L3
+ChangePinName(L3, 1, 1)
+ChangePinName(L3, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element L2
+ChangePinName(L2, 1, 1)
+ChangePinName(L2, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
+
+# Start of element C12
+ChangePinName(C12, 2, 2)
+ChangePinName(C12, 1, 1)
+
+# Start of element C11
+ChangePinName(C11, 2, 2)
+ChangePinName(C11, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element L4
+ChangePinName(L4, 1, 1)
+ChangePinName(L4, 2, 2)
+
+# Start of element L5
+ChangePinName(L5, 1, 1)
+ChangePinName(L5, 2, 2)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element X1
+ChangePinName(X1, 2, 2)
+ChangePinName(X1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 24, SWDCLK)
+ChangePinName(U1, 23, SWDIO/nRESET)
+ChangePinName(U1, 30, VDD_PA)
+ChangePinName(U1, 38, XC2)
+ChangePinName(U1, 37, XC1)
+ChangePinName(U1, 3, P0.30)
+ChangePinName(U1, 48, P0.29)
+ChangePinName(U1, 47, P0.28)
+ChangePinName(U1, 46, P0.27)
+ChangePinName(U1, 45, P0.26)
+ChangePinName(U1, 44, P0.25)
+ChangePinName(U1, 43, P0.24)
+ChangePinName(U1, 42, P0.23)
+ChangePinName(U1, 41, P0.22)
+ChangePinName(U1, 40, P0.21)
+ChangePinName(U1, 28, P0.20)
+ChangePinName(U1, 27, P0.19)
+ChangePinName(U1, 26, P0.18)
+ChangePinName(U1, 25, P0.17)
+ChangePinName(U1, 22, P0.16)
+ChangePinName(U1, 21, P0.15)
+ChangePinName(U1, 20, P0.14)
+ChangePinName(U1, 19, P0.13)
+ChangePinName(U1, 18, P0.12)
+ChangePinName(U1, 17, P0.11)
+ChangePinName(U1, 16, P0.10)
+ChangePinName(U1, 15, P0.09)
+ChangePinName(U1, 14, P0.08)
+ChangePinName(U1, 11, P0.07)
+ChangePinName(U1, 10, P0.06)
+ChangePinName(U1, 9, P0.05)
+ChangePinName(U1, 8, P0.04)
+ChangePinName(U1, 7, P0.03)
+ChangePinName(U1, 6, P0.02)
+ChangePinName(U1, 5, P0.01)
+ChangePinName(U1, 4, P0.00)
+ChangePinName(U1, 2, DCC)
+ChangePinName(U1, 36, AVDD)
+ChangePinName(U1, 35, AVDD)
+ChangePinName(U1, 34, VSS)
+ChangePinName(U1, 33, VSS)
+ChangePinName(U1, 13, VSS)
+ChangePinName(U1, 12, VDD)
+ChangePinName(U1, 1, VDD)
+ChangePinName(U1, 32, ANT2)
+ChangePinName(U1, 31, ANT1)
+ChangePinName(U1, 29, DEC2)
+ChangePinName(U1, 39, DEC1)
